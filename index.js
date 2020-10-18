f018e6ab13ad52d7dc8f486fe2404d6a364e5754
Kris <wizard777@tlen.pl>
2020-10-18 12:51:08 +0200

    Added Sign in disabled, button radio

--- index.js
+++ index.js
@@ -23,7 +23,18 @@
         else {
             formInput.classList.remove('not-empty');
         }
-    };    
+    };
+
+
 })();
 
+           
+function isChecked(checkbox, submit_button) {
+    const myButton = document.querySelector('.btn');
+    if (checkbox.checked == true) {
+        myButton.disabled = false;
+    } else {
+        myButton.disabled = true;
+    };
+}
 
